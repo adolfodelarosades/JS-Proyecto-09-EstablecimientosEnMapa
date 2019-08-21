db0e37843b96dcff67d6bed2057de06eac6b5de5
Adolfo <adolfodelarosa2012@gmail.com>
2019-08-21 19:29:54 +0200

    Versión Producción

---
+++ docs/js/app.js
@@ -0,0 +1,12 @@
+const ui = new UI();
+document.addEventListener('DOMContentLoaded', () => {
+    ui.mostrarEstablecimiento();
+});
+const buscador = document.querySelector('#buscar input');
+buscador.addEventListener('input', () => {    
+    if(buscador.value.length > 5 ){
+        ui.obtenerSugerencias(buscador.value);
+    }else{
+        ui.mostrarEstablecimiento();
+    }
+});
