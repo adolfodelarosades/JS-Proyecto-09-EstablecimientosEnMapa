7522e89174808cf29c28c07c1463c32bc4eac746
Adolfo <adolfodelarosa2012@gmail.com>
2019-08-21 18:40:30 +0200

    Agregando los Pines

--- js/API.js
+++ js/API.js
@@ -1,7 +1,7 @@
 class API{
 
     async obtenerDatos(){
-        const total = 11000;
+        const total = 100;
 
         //Obtener los datos desde la API
         const datos = await fetch(`https://api.datos.gob.mx/v1/precio.gasolina.publico?pageSize=${total}`);
